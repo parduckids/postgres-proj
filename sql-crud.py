e9c486f8aebad0b1337041a517335feba8be1009
Adam V <parduckids@gmail.com>
2024-03-07 22:04:59 +0000

    crud with orm

--- sql-crud.py
+++ sql-crud.py
@@ -1,7 +1,7 @@
 from sqlalchemy import (
     create_engine, Column, Integer, String
 )
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 #executing the instructions from the "chinook" database
@@ -9,18 +9,17 @@ db = create_engine("postgresql:///chinook")
 base = declarative_base()
 
 
+
+
 # create a class-based model for the "Programmer" table
 class Programmer(base):
     __tablename__ = "Programmer"
     id = Column(Integer, primary_key=True)
-    first_name
-    last_name
-    gender
-    nationality
-    famous_for
-
-
-
+    first_name = Column(String)
+    last_name = Column(String)
+    gender = Column(String)
+    nationality = Column(String)
+    famous_for = Column(String)
 
 
 
@@ -32,3 +31,117 @@ session = Session()
 
 # creating the database using declarative_base subclass
 base.metadata.create_all(db)
+
+# creating records on our Programmer table
+ada_lovelace = Programmer(
+    first_name = "Ada",
+    last_name = "Lovelace",
+    gender = "F",
+    nationality = "British",
+    famous_for = "First Programmer"
+)
+alan_turing = Programmer(
+    first_name = "Alan",
+    last_name = "Turing",
+    gender = "M",
+    nationality = "British",
+    famous_for = "Modern Computing"
+)
+
+grace_hopper = Programmer(
+    first_name = "Grace",
+    last_name = "Hopper",
+    gender = "F",
+    nationality = "American",
+    famous_for = "COBOL language"
+)
+margaret_hamilton = Programmer(
+    first_name = "Margaret",
+    last_name = "Hamilton",
+    gender = "F",
+    nationality = "American",
+    famous_for = "Apollo 11"
+)
+bill_gates = Programmer(
+    first_name = "Bill",
+    last_name = "Gates",
+    gender = "M",
+    nationality = "American",
+    famous_for = "Microsoft"
+)
+tim_berners_lee = Programmer(
+    first_name = "Tim",
+    last_name = "Berners_lee",
+    gender = "M",
+    nationality = "British",
+    famous_for = "World Wide Web"
+)
+adam_voros = Programmer(
+    first_name = "Adam",
+    last_name = "Voros",
+    gender = "M",
+    nationality = "British & Hungarian",
+    famous_for = "VVhisper"
+)
+
+# add each instance of our programmers to our session
+# session.add(ada_lovelace)
+# session.add(alan_turing)
+# session.add(grace_hopper)
+# session.add(margaret_hamilton)
+# session.add(bill_gates)
+# session.add(tim_berners_lee)
+# session.add(adam_voros)
+
+
+# update a single record
+# programmer = session.query(Programmer).filter_by(id=7).first()
+# programmer.famous_for = "World President"
+
+# updating multiple records
+# people = session.query(Programmer)
+# for person in people:
+#     if person.gender == "F":
+#         person.gender = "Female"
+#     elif person.gender == "M":
+#         person.gender = "Male"
+#     else:
+#         print("Gender not defined")
+#     # commit our session to the database
+#     session.commit()
+
+
+# deleting a single record
+# fname = input("Enter first name: ")
+# lname = input("Enter last name: ")
+
+# programmer = session.query(Programmer).filter_by(first_name=fname, last_name=lname).first()
+# # defensive programming
+# if programmer is not None:
+#     print("Programmer Found: ", programmer.first_name + " " + programmer.last_name)
+#     confirmation = input("Are you sure you want to delete this record? (y\n) ")
+#     if confirmation.lower() == "y":
+#         session.delete(programmer)
+#         session.commit()
+#         print("Programmer has been deleted")
+#     else:
+#         print("Programmer not deleted")
+        
+# else:
+#     print("No records found")
+
+
+
+
+
+# query the database to find all Programmers
+programmers = session.query(Programmer)
+for programmer in programmers:
+    print(
+        programmer.id,
+        programmer.first_name + " " + programmer.last_name,
+        programmer.gender,
+        programmer.nationality,
+        programmer.famous_for,
+        sep=" | "
+    )
